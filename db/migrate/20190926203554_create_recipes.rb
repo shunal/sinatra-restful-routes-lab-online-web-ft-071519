5c2d3d116f94288a703f54e3b2d2d78485e263d8
Steven Hunal <stevehunal@gmail.com>
2019-09-26 20:39:05 +0000

    Automatically backed up by Learn

--- db/migrate/20190926203554_create_recipes.rb
+++ db/migrate/20190926203554_create_recipes.rb
@@ -3,6 +3,7 @@ class CreateRecipes < ActiveRecord::Migration
     create_table :recipes do |t|
       t.string :name
       t.string :ingredients
-      t.integer
+      t.string :cook_time
+    end
   end
 end
